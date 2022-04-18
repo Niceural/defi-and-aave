98d37bc04a4e787524c4e5a0d53e1d323d257c85
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-04-18 21:18:39 +0200

    done

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,3 +1,4 @@
+from threading import BrokenBarrierError
 from brownie import network, interface, config
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
@@ -18,6 +19,37 @@ def main():
     tx.wait(1)
     print("Deposited!")
     borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    dai_eth_price = get_asset_price(config["networks"][network.show_active()]["dai_eth_price_feed"])
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(dai_address, Web3.toWei(amount_dai_to_borrow, "ether"), 1, 0, account.address, {"from":account})
+    borrow_tx.wait(1)
+    print("We borrowed some DAI!")
+    get_borrowable_data(lending_pool, account)
+    repay_all(Web3.toWei(amount_dai_to_borrow, "ether"), lending_pool, account)
+    print(
+        "You just deposited, borrowed, and repayed with Aave, Brownie, and Chainlink!"
+    )
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(Web3.toWei(amount, "ether"), lending_pool, config["networks"][network.show_active()]["dai_token"], account)
+    repay_tx = lending_pool.repay(
+        config["networks"][network.show_active()]["dai_token"],
+        amount,
+        1,
+        account.address,
+        {"from": account}
+    )
+    repay_tx.wait(1)
+    print("Repayed!")
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[-1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The latest DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
 
 def get_borrowable_data(lending_pool, account):
     (
